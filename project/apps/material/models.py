a9fda4980b5c033ba8d188ae4891bd28838de09b
Pablo Saenz de Buruaga <pablo.saenzde@gmail.com>
2023-05-31 00:15:18 -0300

    actualización de models, views, urls.

--- project/apps/material/models.py
+++ project/apps/material/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Material(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Dimensiones(models.Model):
+    largo = models.CharField(null=True)
+    ancho = models.CharField(max_length=50)
+    espesor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.ancho} {self.espesor}"
+
+
+# Crear un Model más
--- project/apps/material/views.py
+++ project/apps/material/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Material
+
+
+def index(request):
+    material_registros = Material.objects.all()
+    contexto = {"material": material_registros}
+    return render(request, "material/index.html", contexto)
